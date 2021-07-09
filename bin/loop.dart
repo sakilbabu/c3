24b4ebd944d8228235d3ea6d229c92d33cf446d1
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-07-09 07:17:24 -0400

    Learnt Do While

--- bin/loop.dart
+++ bin/loop.dart
@@ -1,7 +1,11 @@
 void main(List<String> args) {
-  int i = 0;
-  while (i / 3 != 2) {
-    print(i);
+  int i = 10;
+
+  do {
+    print("This is gonna be executed at least once $i");
     i++;
-  }
+  
+  } 
+  while (i <= 5);
+
 }
